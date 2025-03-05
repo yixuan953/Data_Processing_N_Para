2a5dcb4dc5afe255497d1eb49f3353635649a1fc
yixuan953 <zhou111@master200.internal.anunna.wur.nl>
2025-03-05 12:07:44 +0100

    20250305 Add EF_N2O calculation

---
+++ Emission/Cal_EF.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+#-----------------------------Mail address-----------------------------
+
+#-----------------------------Output files-----------------------------
+#SBATCH --output=HPCReport/output_%j.txt
+#SBATCH --error=HPCReport/error_output_%j.txt
+
+#-----------------------------Required resources-----------------------
+#SBATCH --time=600
+#SBATCH --mem=250000
+
+#--------------------Environment, Operations and Job steps-------------
+# Step 1 - Calculate EF_N2O 
+module load python/3.12.0
+python /lustre/nobackup/WUR/ESG/zhou111/Code/Data_Processing/N_cycling_Parameters/Emission/1_EF_N2O.py
---
+++ Emission/Readme.txt
@@ -0,0 +1,11 @@
+The gaseous emission contains three parts: NH3, N2O, and NOx. In which;
+
+1. EF_N2O: [0.0019, 0.0071] based on annual precipitation (mm)
+    < 400:       0.0019
+    [400,600):   0.0059
+    [600,800):   0.0071
+    [800,1000):  0.0048
+    [1000,1500): 0.0018
+    >=1500:      0.0024
+
+2. EF_NH3: 
